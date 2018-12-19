db0f61df45dd747f83e92c5a4753ec4a7e9c9726
Meghan Kelly <mkelly22@wisc.edu>
2018-12-19 14:51:40 -0600

    Param and database setup

---
+++ config/param.php
@@ -0,0 +1,14 @@
+<?PHP
+
+$dbtype = 'pgsql';
+$dbhost = '144.92.235.239';
+$dbname = 'mapstudy';
+$dbuser = 'mapstudy';
+$dbpassword = 'MQKg%eMkdY!R%2';
+$smtphost = 'smtp.office365.com';
+$euser = 'mkelly22@wisc.edu';
+$epass = 'Lawrence3002';
+$toaddr = 'mkelly22@wisc.edu';
+$subject = 'MapStudy Response';
+
+?>
